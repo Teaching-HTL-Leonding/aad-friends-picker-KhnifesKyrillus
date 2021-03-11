fa4835f8db9c3d79bcd6cc2917fc15e8254f9321
KhnifesKyrillus <khnifes.kyrillus@gmail.com>
2021-03-11 12:31:17 +0100

    frontend works

---
+++ FriendsPicker_API/FriendsPicker_API/Context/FriendsContext.cs
@@ -0,0 +1,15 @@
+﻿using FriendsPicker_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendsPicker_API.Context
+{
+    public class FriendsContext : DbContext
+    {
+        public FriendsContext(DbContextOptions<FriendsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+    }
+}
---
+++ FriendsPicker_API/FriendsPicker_API/Model/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FriendsPicker_API.Model
+{
+    public class Friend
+    {
+        
+        private int Id { get; set; }
+
+        private String UserId { get; set; }
+        
+        private String FriendId { get; set; }
+    }
+}
